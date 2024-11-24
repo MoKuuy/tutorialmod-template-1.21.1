b35f67f43e20ac85d361be4253d0616f68be2d03
mokuuy <zly1017359849@gmail.com>
2024-11-24 22:55:33 +0800

    items,group,blocks  create study

--- src/main/java/com/bession/tutorialmod/Tutorialmod.java
+++ src/main/java/com/bession/tutorialmod/Tutorialmod.java
@@ -1,5 +1,8 @@
 package com.bession.tutorialmod;
 
+import com.bession.tutorialmod.block.ModBlocks;
+import com.bession.tutorialmod.item.ModItemGroup;
+import com.bession.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -8,17 +11,15 @@ import org.slf4j.LoggerFactory;
 public class Tutorialmod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+		ModItemGroup.registerModItemGroup();
+		ModBlocks.registerModBlock();
+
 	}
 }
